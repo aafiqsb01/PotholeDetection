6bc1e7a28b36b99fc635544e3781141ea16f7e07
aafiqbukhari <aafiqsb786@gmail.com>
2023-03-21 23:26:05 +0000

    Added Button for enabling automatic detection function on Home screen.

--- app/src/main/java/com/example/potholedetectionappv1/HomeFragment.java
+++ app/src/main/java/com/example/potholedetectionappv1/HomeFragment.java
@@ -3,6 +3,8 @@ package com.example.potholedetectionappv1;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,6 +12,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,6 +21,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,6 +48,8 @@ public class HomeFragment extends Fragment implements LocationListener {
     static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
     private String Longitude;
     private String Latitude;
+    private ImageButton activateAutomaticDetection;
+    private boolean paused;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -55,6 +61,24 @@ public class HomeFragment extends Fragment implements LocationListener {
 
         displayUserEmail = (TextView) rootView.findViewById(R.id.userEmailLabel);
         reportPothole = (Button) rootView.findViewById(R.id.reportPothole);
+        activateAutomaticDetection = (ImageButton) rootView.findViewById(R.id.automaticDetection);
+
+        activateAutomaticDetection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int icon;
+
+                if (paused) {
+                    paused = false;
+                    icon = R.drawable.baseline_gps_off_24;
+                } else {
+                    paused = true;
+                    icon = R.drawable.baseline_gps_fixed_24;
+                }
+
+                activateAutomaticDetection.setImageDrawable(ContextCompat.getDrawable(getContext(), icon));
+            }
+        });
 
         spinnerSeverity = (Spinner) rootView.findViewById(R.id.dropdown_Severity);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.Severities, android.R.layout.simple_spinner_item);
